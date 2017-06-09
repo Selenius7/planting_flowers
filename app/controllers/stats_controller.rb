a56e1323bbd8b911954827485ea9e803e3bdb954
Selenius7 <aliaksei.sialenia@gmail.com>
2017-06-09 04:57:54 +0300

    Create stats_controller.rb

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -39,6 +39,26 @@ class StatsController < ApplicationController
               LEFT OUTER JOIN
           orders o ON o.flower_id = f.id
       GROUP BY c.id;
+
+      SELECT 
+        c.name,
+        COUNT(o.flower_id) AS sold_flowers,
+        COUNT(f.id) AS total_flowers,
+        SUM(f.cost) AS income,
+        SUM(d.cost) AS expense,
+        SUM(f.cost) - SUM(d.cost) AS difference
+      FROM
+          cultures c
+              LEFT OUTER JOIN
+          flowers f ON f.culture_id = c.id
+              LEFT OUTER JOIN
+          elevations e ON e.flower_id = f.id
+              LEFT OUTER JOIN
+          garden_beds g ON g.id = e.garden_beds_id
+              LEFT OUTER JOIN
+          orders o ON o.flower_id = f.id
+      GROUP BY c.id;
+    
     SQL
     @statistics = ActiveRecord::Base.connection.select_rows(sql).to_a
   end
